7636b4f2b6ea19e0dea4111b854581e653d29582
irbgeo <morochevgm@gmail.com>
2023-05-07 18:16:01 +0400

    del tci

--- bb.go
+++ bb.go
@@ -2,6 +2,7 @@ package ta
 
 import "github.com/shopspring/decimal"
 
+// BB - Bollinger Bands.
 func BB(series Series, period, multi int) (middle Series, upper Series, lower Series) {
 	middle = SMA(series, period)
 	dev := StDev(series, period).MulConst(decimal.NewFromInt(int64(multi)))
