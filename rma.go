4c8dc120cc26afc604b11241541cd2ac0f3b816f
irbgeo <morochevgm@gmail.com>
2023-05-26 16:04:48 +0400

    comments to rma

--- rma.go
+++ rma.go
@@ -3,20 +3,25 @@ package ta
 import "github.com/shopspring/decimal"
 
 // RMA - Rolling Moving Average
+// rma[0] = src[0]
+// alpha+src[i]+(1-alpha)+rma[i-1]
+// alpha = 1 / period
 func RMA(src Series, period int) Series {
-	out := make(Series, 0, len(src))
-	out = append(out, src[0].Copy())
+	rma := make(Series, 0, len(src))
+	rma = append(rma, src[0].Copy())
 
 	alpha := one.Div(decimal.NewFromInt(int64(period)))
 
 	for i, v := range src[1:] {
-		out = append(out,
+		rma = append(rma,
 			Value{
-				Time:  v.Time,
-				Value: alpha.Mul(v.Value).Add(one.Sub(alpha).Mul(out[i].Value)),
+				Time: v.Time,
+				// alpha+src[i]+(1-alpha)+out[i-1]
+				// https://download.esignal.com/products/workstation/help/charts/studies/rmi.htm
+				Value: alpha.Mul(v.Value).Add(one.Sub(alpha).Mul(rma[i].Value)),
 			},
 		)
 	}
 
-	return out
+	return rma
 }
