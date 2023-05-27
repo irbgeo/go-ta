56fc6a91fcbb017c80352e5276749f830ec6de6a
irbgeo <morochevgm@gmail.com>
2023-05-27 19:18:27 +0400

    return

--- rma_test.go
+++ rma_test.go
@@ -1,102 +1,67 @@
 package ta
 
-import "github.com/shopspring/decimal"
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+)
 
 var (
-	testPeriod = 0.5
-
-	// smoothing factor / n / period = 0.5
-
-	testRmaSet = []Value{
+	expectedRMA = []Value{
 		{
 			Time:  1,
-			Value: decimal.NewFromFloat(100),
+			Value: decimal.NewFromFloat(7),
 		},
 		{
 			Time:  2,
-			Value: decimal.NewFromFloat(105),
+			Value: decimal.NewFromFloat(7.55),
 		},
 		{
 			Time:  3,
-			Value: decimal.NewFromFloat(110),
+			Value: decimal.NewFromFloat(5.166433333333333),
 		},
 		{
 			Time:  4,
-			Value: decimal.NewFromFloat(115),
+			Value: decimal.NewFromFloat(4.034688888888889),
 		},
 		{
 			Time:  5,
-			Value: decimal.NewFromFloat(120),
+			Value: decimal.NewFromFloat(1.3472259259259258),
 		},
 		{
 			Time:  6,
-			Value: decimal.NewFromFloat(125),
+			Value: decimal.NewFromFloat(300.89815061728393),
 		},
 		{
 			Time:  7,
-			Value: decimal.NewFromFloat(130),
+			Value: decimal.NewFromFloat(202.9321004115226),
 		},
 		{
 			Time:  8,
-			Value: decimal.NewFromFloat(135),
+			Value: decimal.NewFromFloat(136.1914002743484),
 		},
 		{
 			Time:  9,
-			Value: decimal.NewFromFloat(140),
+			Value: decimal.NewFromFloat(89.89093351623228),
 		},
 		{
 			Time:  10,
-			Value: decimal.NewFromFloat(145),
+			Value: decimal.NewFromFloat(61.830622344154854),
 		},
 		{
 			Time:  11,
-			Value: decimal.NewFromFloat(155),
+			Value: decimal.NewFromFloat(66.92374822943657),
 		},
 	}
+)
 
-var expectedRMA = []Value{
-	{
-		Time:  1,
-		Value: decimal.NewFromFloat(100.000000),
-	},
-	{
-		Time:  2,
-		Value: decimal.NewFromFloat(103.333333),
-	},
-	{
-		Time:  3,
-		Value: decimal.NewFromFloat(107.142857),
-	},
-	{
-		Time:  4,
-		Value: decimal.NewFromFloat(111.333333),
-	},
-	{
-		Time:  5,
-		Value: decimal.NewFromFloat(115.806452),
-	},
-	{
-		Time:  6,
-		Value: decimal.NewFromFloat(120.476190),
-	},
-	{
-		Time:  7,
-		Value: decimal.NewFromFloat(125.275591),
-	},
-	{
-		Time:  8,
-		Value: decimal.NewFromFloat(130.156863),
-	},
-	{
-		Time:  9,
-		Value: decimal.NewFromFloat(135.088063),
-	},
-	{
-		Time:  10,
-		Value: decimal.NewFromFloat(140.048876),
-	},
-	{
-		Time:  11,
-		Value: decimal.NewFromFloat(147.528090),
-	},
+func TestRMA(t *testing.T) {
+	actualRMA := RMA(testSrcSet, testPeriod)
+
+	require.Equal(t, len(expectedRMA), len(actualRMA), "compare len")
+	for i := 0; i < len(actualRMA); i++ {
+		require.Equalf(t, expectedRMA[i].Time, actualRMA[i].Time, "compare time: %d", i+1)
+		require.Equalf(t, expectedRMA[i].Value.Round(4).String(), actualRMA[i].Value.Round(4).String(), "compare : %d", i+1)
+	}
 }
