846b6b25ad02de1213073da2dd51a4da24d5a55f
danil_proskurin <danielpashtetov@gmail.com>
2023-07-06 11:55:47 +0300

    stdev test values

--- stdev_test.go
+++ stdev_test.go
@@ -9,52 +9,52 @@ import (
 )
 
 var (
-	testPeriodStDev = 3
+	testPeriodStDev = 2
 
 	testSrcSetStDev = []ta.Value{
 		{
 			Time:  1,
-			Value: decimal.NewFromFloat(7),
+			Value: decimal.NewFromFloat(7.5),
 		},
 		{
 			Time:  2,
-			Value: decimal.NewFromFloat(8.1),
+			Value: decimal.NewFromFloat(8.4),
 		},
 		{
 			Time:  3,
-			Value: decimal.NewFromFloat(0.3993),
+			Value: decimal.NewFromFloat(0.9943),
 		},
 		{
 			Time:  4,
-			Value: decimal.NewFromFloat(1.7712),
+			Value: decimal.NewFromFloat(2.7712),
 		},
 		{
 			Time:  5,
-			Value: decimal.NewFromFloat(-4.0277),
+			Value: decimal.NewFromFloat(-1.0277),
 		},
 		{
 			Time:  6,
-			Value: decimal.NewFromFloat(900),
+			Value: decimal.NewFromFloat(950),
 		},
 		{
 			Time:  7,
-			Value: decimal.NewFromFloat(7),
+			Value: decimal.NewFromFloat(72.5),
 		},
 		{
 			Time:  8,
-			Value: decimal.NewFromFloat(2.71),
+			Value: decimal.NewFromFloat(2.91),
 		},
 		{
 			Time:  9,
-			Value: decimal.NewFromFloat(-2.71),
+			Value: decimal.NewFromFloat(-2.721),
 		},
 		{
 			Time:  10,
-			Value: decimal.NewFromFloat(5.71),
+			Value: decimal.NewFromFloat(9.71),
 		},
 		{
 			Time:  11,
-			Value: decimal.NewFromFloat(77.11),
+			Value: decimal.NewFromFloat(97.11),
 		},
 	}
 )
@@ -66,43 +66,43 @@ var expectedStDev = []ta.Value{
 	},
 	{
 		Time:  2,
-		Value: decimal.NewFromFloat(0),
+		Value: decimal.NewFromFloat(0.45000),
 	},
 	{
 		Time:  3,
-		Value: decimal.NewFromFloat(5.1664333333333333),
+		Value: decimal.NewFromFloat(3.70285),
 	},
 	{
 		Time:  4,
-		Value: decimal.NewFromFloat(4.0346888888888889),
+		Value: decimal.NewFromFloat(0.88845),
 	},
 	{
 		Time:  5,
-		Value: decimal.NewFromFloat(1.3472259259259259),
+		Value: decimal.NewFromFloat(1.89945),
 	},
 	{
 		Time:  6,
-		Value: decimal.NewFromFloat(300.8981506172839506),
+		Value: decimal.NewFromFloat(475.51385),
 	},
 	{
 		Time:  7,
-		Value: decimal.NewFromFloat(202.9321004115226337),
+		Value: decimal.NewFromFloat(438.75000),
 	},
 	{
 		Time:  8,
-		Value: decimal.NewFromFloat(136.1914002743484225),
+		Value: decimal.NewFromFloat(34.79500),
 	},
 	{
 		Time:  9,
-		Value: decimal.NewFromFloat(89.8909335162322817),
+		Value: decimal.NewFromFloat(2.81550),
 	},
 	{
 		Time:  10,
-		Value: decimal.NewFromFloat(61.8306223441548545),
+		Value: decimal.NewFromFloat(6.21550),
 	},
 	{
 		Time:  11,
-		Value: decimal.NewFromFloat(66.9237482294365697),
+		Value: decimal.NewFromFloat(),
 	},
 }
 
